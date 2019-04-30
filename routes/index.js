cde2a8248e2191cf7f52dab2d41f94c0eea7f2b7
averastlast <carapowpowell@gmail.com>
2019-04-30 00:18:44 -0400

    trying to see

--- routes/index.js
+++ routes/index.js
@@ -1,31 +1,12 @@
 let express = require('express')
 let router = express.Router()
-const AxisController = require('../controllers/axis.js')
-// const AltsController = require('../controllers/alts.js')
-// const CommentsController = require('../controllers/comments.js')
 
 // Create a GET index route "/" that triggers the controller index function
-router.get('/', AxisController.index)
+//home
 
-// Create a GET new route "/new" that triggers the controller new function
-router.get('/:new', AxisController.new)
-
-// // Create a GET show route "/:id" that triggers the controller show function
-// router.get('/:id', AxisController.show)
-
-// // Create a POST index route "/" that triggers the controller create function
-// router.post('/', AxisController.create)
-
-// // Create a GET edit route "/:id/edit" that triggers the controller edit function
-// router.get("/:id/edit", AxisController.edit)
-
-// // Create a PUT update route "/:id" that triggers the controller update function
-// router.put('/:id', AxisController.update)
-
-// //buy
-// router.put('/:id/buy', AxisController.buy)
-
-// // Create a DELETE delete route "/:id" that triggers the controller delete function
-// router.delete('/:id',AxisController.delete)
+router.get('/', function(req, res,) {
+    res.render('index');
+  });
+  
 
 module.exports = router

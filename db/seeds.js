0e851a31dcde536543e5e6389bb28de8994a9fda
averastlast <carapowpowell@gmail.com>
2019-05-01 13:54:46 -0400

    seeds

--- db/seeds.js
+++ db/seeds.js
@@ -20,30 +20,49 @@
 //   })
 
 
-let Alts = require('../models/Alts.js')
-// working, ran once
-let newAlts = [
-  {
-    alias: 'angel',
-    game: 'halo',
-    platform: 'PC',
-    hours: 8,
-    youtubeLink: 'link',
-    forSale: true,
-    price: 400
-  },
-  {
-    alias: 'devil',
-    game: 'CoD',
-    platform: 'PS4',
-    hours: 10,
-    youtubeLink: 'link2',
-    forSale: false,
-    price: 0
-  }
-]
+// let Alts = require('../models/Alts.js')
+// // working, ran once
+// let newAlts = [
+//   {
+//     alias: 'angel',
+//     game: 'halo',
+//     platform: 'PC',
+//     hours: 8,
+//     youtubeLink: 'link',
+//     forSale: true,
+//     price: 400
+//   },
+//   {
+//     alias: 'devil',
+//     game: 'CoD',
+//     platform: 'PS4',
+//     hours: 10,
+//     youtubeLink: 'link2',
+//     forSale: false,
+//     price: 0
+//   }
+// ]
 
-Alts.create(newAlts).then(alts => {
-    console.log('Saved Alts', alts)
-  })
+// Alts.create(newAlts).then(alts => {
+//     console.log('Saved Alts', alts)
+//   })
 
+
+
+//   let Comments = require('../models/Comments.js')
+// let newComments = [
+//   {
+//     alias: 'Greenthyme',
+//     game: 'BOXMAN',
+//     feedback: 'Rools'
+//   },
+//   {
+//     alias: 'Redthyme',
+//     game: 'BOXMAN 2',
+//     feedback: 'Drools'
+//   }
+// ]
+
+// Comments.create(newComments).then(comments => {
+//     console.log('Saved Commentss', comments)
+//   })
